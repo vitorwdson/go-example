9a9cc5cc96d06ee6000fc455f2196e2c6af68e26
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-26 11:22:34 -0400

    Returning a random greeting (using slices)

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
@@ -10,6 +12,24 @@ func Hello(name string) (string, error) {
 		return "", errors.New("Empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomGreeting(), name)
 	return message, nil
+}
+
+// The init function will always run on the program startup
+func init() {
+	// Setting the seed for the rand module
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomGreeting() string {
+	// Creating a slice (an dynamically sized array) of greeting formats
+	greetings := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return an random index of the slice
+	return greetings[rand.Intn(len(greetings))]
 }
