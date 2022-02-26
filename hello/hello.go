82af81e7e8aab109e314ff812ad8c5454bfd4cdf
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-26 11:09:22 -0400

    Error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Vitor")
-	fmt.Println(message)
+	log.SetPrefix("greetings: ") // Prefix the error output with "greetings: "
+	log.SetFlags(0) // Disable printing the time and line number on errors
+
+	name := "Vitor"
+	message, err := greetings.Hello(name) // Extracting both the message and error
+
+	if err != nil { // Checking if an error occurred
+		log.Fatal(err) // Prints the error and exits the program
+	}
+
+	fmt.Println(message) // If no error happened, print the message
 }
